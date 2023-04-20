aab30429941a4dcd1bb074bdcf6e9c939f28ada4
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2023-04-20 14:34:16 -0500

    fix quoting in control
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- controls/SV-204479.rb
+++ controls/SV-204479.rb
@@ -3,19 +3,19 @@ control 'SV-204479' do
     correctly labeled to prevent unauthorized modification.'
   desc 'If an unauthorized or modified device is allowed to exist on the system, there is the possibility the system
     may perform unintended or unauthorized operations.'
-  desc 'check', 'Verify that all system device files are correctly labeled to prevent unauthorized modification.
+  desc 'check', %q(Verify that all system device files are correctly labeled to prevent unauthorized modification.
 
 List all device files on the system that are incorrectly labeled with the following commands:
 
 Note: Device files are normally found under "/dev", but applications may place device files in other directories and may necessitate a search of the entire system.
 
-#find /dev -context *:device_t:* \\( -type c -o -type b \\) -printf "%p %Z\\n"
+#find /dev -context *:device_t:* \( -type c -o -type b \) -printf "%p %Z\n"
 
-#find /dev -context *:unlabeled_t:* \\( -type c -o -type b \\) -printf "%p %Z\\n"
+#find /dev -context *:unlabeled_t:* \( -type c -o -type b \) -printf "%p %Z\n"
 
 Note: There are device files, such as "/dev/vmci", that are used when the operating system is a host virtual machine. They will not be owned by a user on the system and require the "device_t" label to operate. These device files are not a finding.
 
-If there is output from either of these commands, other than already noted, this is a finding.'
+If there is output from either of these commands, other than already noted, this is a finding.)
   desc 'fix', 'Run the following command to determine which package owns the device file:
 
     # rpm -qf <filename>
