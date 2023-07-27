87db3d6d8f1f3bb73e96fa95097be736e715a2e5
Sumaa Sayed <ssayed1@umbc.edu>
2023-07-27 11:15:24 -0400

    fixed errors found in SV-204409
    Signed-off-by: Sumaa Sayed <ssayed1@umbc.edu>

--- controls/SV-204409.rb
+++ controls/SV-204409.rb
@@ -8,11 +8,11 @@ control 'SV-204409' do
     the password, the greater the number of possible combinations that need to be tested before the password is
     compromised.'
   desc 'check', "Note: The value to require a number of numeric characters to be set is expressed as a negative
-    number in "/etc/security/pwquality.conf".
-    Check the value for "dcredit" in "/etc/security/pwquality.conf" with the following command:
+    number in \"/etc/security/pwquality.conf\".
+    Check the value for \"dcredit\" in \"/etc/security/pwquality.conf\" with the following command:
     # grep dcredit /etc/security/pwquality.conf
     dcredit = -#{input('min_numeric_characters')}
-    If the value of "dcredit" is not set to a negative value, this is a finding."
+    If the value of \"dcredit\" is not set to a negative value, this is a finding."
   desc 'fix', "Configure the operating system to enforce password complexity by requiring that at least #{input('min_numeric_characters')} numeric
     character be used by setting the \"dcredit\" option.
     Add the following line to /etc/security/pwquality.conf (or modify the line to have the required value):
